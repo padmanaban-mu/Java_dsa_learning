6b53541e651bb0e31811f017c7d04a52eafff8d8
Padmanaban <Padmanabanm002@gmail.com>
2026-07-14 15:22:36 +0530

    Assignment 2

--- Java/src/Basics/_2_Conditional_loops/findFactor.java
+++ Java/src/Basics/_2_Conditional_loops/findFactor.java
@@ -1,4 +1,17 @@
 package Basics._2_Conditional_loops;
 
+import java.util.Scanner;
+
 public class findFactor {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
