3a3dc82cd137f2f4637162a0e0bd3255fb2fd4b7
Padmanaban <Padmanabanm002@gmail.com>
2026-07-17 20:30:40 +0530

    oneD

--- Java/src/Basics/_4_Arrays/OneD.java
+++ Java/src/Basics/_4_Arrays/OneD.java
@@ -1,17 +1,22 @@
 package Basics._4_Arrays;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class OneD {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        System.out.println(n);
         int []arr=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+        Arrays.sort(arr);
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
+        System.out.println(Arrays.toString(arr));
+        String s="hello";
+        System.out.println(s);
     }
 }
